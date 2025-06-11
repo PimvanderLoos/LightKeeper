fcec0bee0a161f06ecef0ab0bb65717584601974
Pim van der Loos <pimvanderloos@gmail.com>
2025-06-11 14:00:05 +0200

    Resolve directories

--- lightkeeper-maven-plugin/src/main/java/nl/pim16aap2/lightkeeper/maven/serverprovider/PaperServerProvider.java
+++ lightkeeper-maven-plugin/src/main/java/nl/pim16aap2/lightkeeper/maven/serverprovider/PaperServerProvider.java
@@ -1,14 +1,15 @@
 package nl.pim16aap2.lightkeeper.maven.serverprovider;
 
+import nl.pim16aap2.lightkeeper.maven.ServerSpecification;
 import org.apache.maven.plugin.logging.Log;
 
 public class PaperServerProvider extends ServerProvider
 {
-    private static final String SERVER_NAME = "Paper";
+    private static final String SERVER_NAME = "paper";
 
-    public PaperServerProvider(Log log)
+    public PaperServerProvider(Log log, ServerSpecification serverSpecification)
     {
-        super(log, SERVER_NAME);
+        super(log, SERVER_NAME, serverSpecification);
         throw new UnsupportedOperationException("Paper server provider is not yet implemented.");
     }
 }
--- lightkeeper-maven-plugin/src/main/java/nl/pim16aap2/lightkeeper/maven/serverprovider/SpigotServerProvider.java
+++ lightkeeper-maven-plugin/src/main/java/nl/pim16aap2/lightkeeper/maven/serverprovider/SpigotServerProvider.java
@@ -1,5 +1,7 @@
 package nl.pim16aap2.lightkeeper.maven.serverprovider;
 
+import lombok.ToString;
+import nl.pim16aap2.lightkeeper.maven.ServerSpecification;
 import org.apache.maven.plugin.logging.Log;
 
 /**
@@ -7,12 +9,19 @@ import org.apache.maven.plugin.logging.Log;
  * <p>
  * This implementation downloads {@code BuildTools.jar} and builds the Spigot server JAR file.
  */
+@ToString(callSuper = true)
 public class SpigotServerProvider extends ServerProvider
 {
-    private static final String SERVER_NAME = "Spigot";
+    private static final String SERVER_NAME = "spigot";
 
-    public SpigotServerProvider(Log log)
+    public SpigotServerProvider(Log log, ServerSpecification serverSpecification)
     {
-        super(log, SERVER_NAME);
+        super(log, SERVER_NAME, serverSpecification);
+    }
+
+    @Override
+    public void prepareServer()
+    {
+        log().info("Preparing spigot server with provider: \n" + this);
     }
 }
