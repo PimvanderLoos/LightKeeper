b7e34e17221ac95d34401ef05e863e2e474e33d7
Pim van der Loos <pimvanderloos@gmail.com>
2026-02-13 12:16:23 +0100

    Give nms parent directory same name as module

--- lightkeeper-maven-plugin-test/src/test/java/nl/pim16aap2/lightkeeper/maven/test/LightkeeperFreshServerIT.java
+++ lightkeeper-maven-plugin-test/src/test/java/nl/pim16aap2/lightkeeper/maven/test/LightkeeperFreshServerIT.java
@@ -1,12 +1,12 @@
 package nl.pim16aap2.lightkeeper.maven.test;
 
 import nl.pim16aap2.lightkeeper.framework.FreshServer;
-import nl.pim16aap2.lightkeeper.framework.LightkeeperExtension;
 import nl.pim16aap2.lightkeeper.framework.ILightkeeperFramework;
+import nl.pim16aap2.lightkeeper.framework.LightkeeperExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(LightkeeperExtension.class)
 @FreshServer
@@ -15,8 +15,6 @@ class LightkeeperFreshServerIT
     @Test
     void mainWorld_shouldStartFreshServerWhenClassIsAnnotatedWithFreshServer(ILightkeeperFramework framework)
     {
-        // setup
-
         // execute
         final var mainWorld = framework.mainWorld();
 
